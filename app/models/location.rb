5bb90b3649081e91734567c38fda78645ad30641
mikka <mikkanthrope@gmail.com>
2018-01-16 23:22:20 -0800

    Add #set_lonlat and #nearby on Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,17 @@
 class Location < ApplicationRecord
 
-  # set_rgeo_factory_for_column(:latlon, RGeo::Geographic.spherical_factory(:srid => 4326))
+  attr_reader :lonlat
+
+  has_many :reviews
+
+  def set_lonlat(lon, lat)
+    factory = RGeo::Geographic.spherical_factory(srid: 4326)
+    self.lonlat = factory.point(lon, lat)
+    self.save
+  end
+
+  def nearby(latitude, longitude, distance)
+    Location.where("ST_DWithin(locations.lonlat, ST_GeographyFromText('SRID=4326;POINT(:lon :lat)'), :distance)", lon: longitude, lat: latitude, distance: distance)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117041023) do
+ActiveRecord::Schema.define(version: 20180117045325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20180117041023) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
-    t.point "latlon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}, null: false
   end
 
 end
