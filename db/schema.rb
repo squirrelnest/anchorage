9c8af68a44ce648a30d8ddc65640ffef5310aba3
mikka <mikkanthrope@gmail.com>
2018-01-20 10:55:08 -0800

    Add admin column to users table and hide admin features from UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120075611) do
+ActiveRecord::Schema.define(version: 20180120184644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180120075611) do
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
