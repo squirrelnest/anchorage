1546470a5fc50b158d78566969797536090501d7
mikka <mikkanthrope@gmail.com>
2018-01-24 18:36:56 -0800

    Add bootstrap and Mapbox map

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,13 +1,5 @@
 class Location < ApplicationRecord
 
-  # ActiveRecord makes an attr_accessor for every single column in the model's database table
-  # Instance variables only live in memory. The object they represent can only live beyond the instance variable if the object is saved/persisted to the db
-  # An instance variable, is a variable unique to a specific instance of an object that may or may not have been persisted to the database yet.
-  # If the data hasn't been persisted yet, the instance of an object (qua the object in memory) may not have the same as attributes as the row/record it represents in the database.
-  # An object is a specific instance of a class.
-  # Objects don't get saved to the db. Only their attributes get saved to the db so you can recreate the object later.
-  # Columns are cheap
-
   attr_writer :lon, :lat
 
   has_many :reviews
