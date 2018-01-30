29b269ceba492daeb13e825fb22fbd96580b9d9e
mikka <mikkanthrope@gmail.com>
2018-01-30 14:55:38 -0800

    DRYed up locations show page and added Bootstrap table styling

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -19,14 +19,14 @@ $(document).on('turbolinks:load', function() {
   }));
 
   // Get coordinates at mouse position
-  map.on('mousemove', function (e) {
-    document.getElementById('info').innerHTML =
-      // e.point is the x, y coordinates of the mousemove event relative
-      // to the top-left corner of the map
-      JSON.stringify(e.point) + '<br />' +
-      // e.lngLat is the longitude, latitude geographical position of the event
-      JSON.stringify(e.lngLat);
-  });
+  // map.on('mousemove', function (e) {
+  //   document.getElementById('info').innerHTML =
+  //     // e.point is the x, y coordinates of the mousemove event relative
+  //     // to the top-left corner of the map
+  //     JSON.stringify(e.point) + '<br />' +
+  //     // e.lngLat is the longitude, latitude geographical position of the event
+  //     JSON.stringify(e.lngLat);
+  // });
 
   // Define points to mark with markers
   const url = '/locations/geojson';
