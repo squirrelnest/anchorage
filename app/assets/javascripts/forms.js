df29e9dfb4330be1096af0395c0aaa8b2455678f
mikka <mikkanthrope@gmail.com>
2018-01-31 14:28:12 -0800

    Create sliding container for review form

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,5 +1,11 @@
 //   /locations/new/${e.lngLat.lng}/${e.lngLat.lat}
 
 function createReview() {
+  event.preventDefault();
+  $('#review-form').css("left", "50%");
+}
 
+function closeForm() {
+  event.preventDefault();
+  $('#review-form').css("left", "100%");
 }
