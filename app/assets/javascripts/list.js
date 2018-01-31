1a0b66b15bb1a8391d3b4532e4f1fb4f6474772d
mikka <mikkanthrope@gmail.com>
2018-01-30 19:36:39 -0800

    Add mouseover event on locations to invoke showReviews()

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -11,8 +11,20 @@ function showLocations(event) {
     data.locations.forEach(function(location) {
       // location = new Location(locationData);
       // html = location.toHtml();
-      html = `<strong><a href='/locations/${location.id}'>${location.nickname}</a></strong> ${location.lat}, ${location.lon} <br />`;
+      html = `<div id='review' data-id='${location.id}' onmouseover='showReviews(event)' onmouseout='hideReviews(event)'><strong><a href='/locations/${location.id}' data-id='${location.id}'>${location.nickname}</a></strong> ${location.lat}, ${location.lon} <br /></div>`;
       $(`[id='country-list-${country}']`).append(html);
     });
   });
 };
+
+function showReviews(event) {
+  let location_id = event.target.attributes['data-id'].nodeValue;
+  $.get(`/locations/${location_id}.json`, function(data) {
+    console.log(data);
+    $('#overlay').html(JSON.stringify(data));
+  });
+}
+
+function hideReviews(event) {
+  $('#overlay').empty();
+}
