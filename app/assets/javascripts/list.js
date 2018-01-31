ab59cb464abc50c1387744486d0396406cb40b3a
mikka <mikkanthrope@gmail.com>
2018-01-31 12:17:30 -0800

    Show reviews when hovering over location

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -11,7 +11,7 @@ function showLocations(event) {
     data.locations.forEach(function(location) {
       // location = new Location(locationData);
       // html = location.toHtml();
-      html = `<div id='review' data-id='${location.id}' onmouseover='showReviews(event)' onmouseout='hideReviews(event)'><strong><a href='/locations/${location.id}' data-id='${location.id}'>${location.nickname}</a></strong> ${location.lat}, ${location.lon} <br /></div>`;
+      html = `<div id='review' data-id='${location.id}' onmouseover='showReviews(event)' onmouseout='hideReviews(event)'><p><strong><a href='/locations/${location.id}' data-id='${location.id}'>${location.nickname}</a></strong><br /> ${location.lat}, ${location.lon} </p></div>`;
       $(`[id='country-list-${country}']`).append(html);
     });
   });
@@ -19,12 +19,17 @@ function showLocations(event) {
 
 function showReviews(event) {
   let location_id = event.target.attributes['data-id'].nodeValue;
+  $('#overlay-container').css("background-color", "#8495a5");
   $.get(`/locations/${location_id}.json`, function(data) {
-    console.log(data);
-    $('#overlay').html(JSON.stringify(data));
+    console.log(data)
+    data.reviews.forEach(function(review) {
+      html = `<div class="review-preview row"><p class="review-content">"${review.content}"</p><p>Stability rating: ${review.stability}</p><p>Reviewed: ${review.date_visited}</p></div>`;
+      $('#overlay').append(html);
+    });
   });
 }
 
 function hideReviews(event) {
   $('#overlay').empty();
+  $('#overlay-container').css("background-color", "#212121");
 }
--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,4 +1,5 @@
 class LocationSerializer < ActiveModel::Serializer
   attributes :id, :nickname, :lonlat, :country, :lat, :lon
   has_many :reviews
+  has_many :users, through: :reviews
 end
