c017d5dfd58297e2ba519d5b9733c8bbbe28267f
mikka <mikkanthrope@gmail.com>
2018-02-01 14:56:27 -0800

    Add headings and buttons to review preview

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -20,10 +20,20 @@ function showReviews(event) {
   let location_id = event.target.attributes['data-id'].nodeValue;
   $('#overlay-container').css("background-color", "#8495a5");
   $.get(`/locations/${location_id}.json`, function(data) {
+    // add DOM element for nickname to overlay div
+    let heading = `<h2>${data.nickname.toUpperCase()}</h2>`
+    $('#overlay').append(heading);
+    // add DOM element for each review to overlay div
     data.reviews.forEach(function(review) {
-      html = `<div class="review-preview row"><p class="review-content">"${review.content}"</p><p>Stability rating: ${review.stability}</p><p>Reviewed: ${review.date_visited}</p></div>`;
+      html = `<div class="review-preview">
+      <p class="review-content">"${review.content}"</p>
+      <p>Stability rating: ${review.stability}</p>
+      <p>Reviewed: ${review.date_visited}</p>
+      </div>`;
       $('#overlay').append(html);
     });
+    let buttons = `<button onClick="createReview(event)">Add Review</button>`
+    $('#overlay').append(buttons);
   });
 }
 
